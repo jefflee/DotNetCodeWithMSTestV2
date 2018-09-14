addda12d46d8c96fedd82b4fd97ecf8632d4bcfb
Jeff Lee <hellow.jeff@gmail.com>
2018-09-14 23:35:13 +0800

    run additional code before/after test case.

---
+++ GameEngine.Tests/Assembly.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GameEngine.Tests
+{
+    [TestClass]
+    public class Assembly
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            Console.WriteLine("AssemblyInit");
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyClean()
+        {
+            Console.WriteLine("AssemblyClean");
+        }
+    }
+}
---
+++ GameEngine.Tests/Lifecycle.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GameEngine.Tests
+{
+    [TestClass]
+    public class Lifecycle
+    {
+        static string SomeTestContext;
+
+        [TestInitialize]
+        public void LifecycleInit()
+        {
+            Console.WriteLine("    TestInitialize Lifecycle");
+        }
+
+        [TestCleanup]
+        public void LifecycleClean()
+        {
+            Console.WriteLine("    TestCleanup Lifecycle");
+        }
+
+        [ClassInitialize]
+        public static void LifecycleClassInit(TestContext context)
+        {
+            Console.WriteLine("  ClassInitialize Lifecycle");
+
+            Console.WriteLine("  data loaded from disk or some expensive object creation");
+            SomeTestContext = "42";
+        }
+
+        [ClassCleanup]
+        public static void LifecycleClassClean()
+        {
+            Console.WriteLine("  ClassCleanup Lifecycle");
+        }
+
+
+        [TestMethod]
+        public void TestA()
+        {
+            Console.WriteLine("      Test A starting");
+            Console.WriteLine($"      Shared test context: {SomeTestContext}");
+        }
+
+
+        [TestMethod]
+        public void TestB()
+        {
+            Console.WriteLine("      Test B starting");
+            Console.WriteLine($"      Shared test context: {SomeTestContext}");
+        }
+    }
+}
